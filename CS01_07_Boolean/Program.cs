3a27fd8fb3da01f948eaeb2ffd2c6cb9b92cc844
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-08-06 16:41:34 +0300

    Boolean

---
+++ CS01_07_Boolean/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace CS01_07_Boolean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Boolean
+            /*
+             * Değerleri kullanıcıdan al        / Get values from user
+             * Değerleri değişkenlere kayıt et  / Save values to variables
+             * Değerleri kontrol et             / Check values
+             * Değerleri veritabanından doğrula / Validate values from database
+             * Evet/Yes  => True
+             * Hayır/No  => False
+             */
+
+            bool userLogInResult = true;
+            Console.WriteLine($"User Login Result is : {userLogInResult}");
+            
+            userLogInResult = false;
+            Console.WriteLine($"User Login Result is : {userLogInResult}");
+
+            // Default value is false
+            // Varsayılan değer FALSE
+            bool userValue; 
+            // If run the project in debug mode you can see the default value is false
+            // Projeyi hata ayıklama modunda çalıştırırsanız, varsayılan değerin false olduğunu görebilirsiniz.
+            
+            // Assign a value to the variable
+            // Değişkene değer atama
+            userValue = true;
+
+            #endregion
+
+        }
+    }
+}
+
