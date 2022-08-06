f9d9a6ef572649b6254bd8451ee7d346b48a207e
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-08-06 20:53:51 +0300

    Object Type

---
+++ CS01_14_ObjectType/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CS01_14_ObjectType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * OBJECT tüm tipleri kapsayan bir tiptir. Bütün tipleri kendi içerisinde muhafaza edebilir.
+             * OBJECT is a type that covers all types. It can keep all types within itself.
+             * object{
+             *      Integer,
+             *      Decimal,
+             *      String,
+             *      Bool,
+             *      Float,
+             *      Double,
+             *      Class,
+             *      Custom Class
+             *  }
+             */
+            object o1 = 100;
+            object o2 = 10.90;
+            object o3 = "C#";
+        }
+    }
+}
