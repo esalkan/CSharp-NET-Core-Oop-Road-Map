00b31cde8bf9e9c65aee0feba2fbc7c9bcb82b34
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-08-06 16:12:05 +0300

    Variables

---
+++ CS01_02_Variables/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CS01_02_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Variables
+            Console.WriteLine("-----> Variables <-----");
+            // variableType variableName = value
+            // degiskenTipi degiskenAdi = atananDeger
+
+            // string           : VariableType
+            // nameSurname      : VariableName
+            // Eyüp Sabri ALKAN : value
+            string nameSurname = "Eyüp Sabri ALKAN";
+
+            Console.WriteLine(nameSurname); // print-out on console.
+            #endregion
+        }
+    }
+}
