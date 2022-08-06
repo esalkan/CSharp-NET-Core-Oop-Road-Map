4a94b4ab83bf56f7bb8c1058671b5339ce6a7a99
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-08-06 16:15:53 +0300

    String Variable Type & String Methods

---
+++ CS01_03_StringVariableTypeAndMethods/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CS01_03_StringVariableTypeAndMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region String Variable Type & String Methods
+            string nameSurname = "Eyüp Sabri ALKAN";
+            Console.WriteLine("-----> String Variable Type <-----");
+            // String Type
+            nameSurname = "Ayşe ALKAN"; // Change the nameSurname Value
+
+            Console.WriteLine("-----> String Methods <-----");
+            string toUpper = nameSurname.ToUpper();
+            string toLower = nameSurname.ToLower();
+            string subString = nameSurname.Substring(2, 5);
+            Console.WriteLine(nameSurname.GetType());
+
+            Console.WriteLine(toUpper);
+            Console.WriteLine(toLower);
+            Console.WriteLine(subString);
+            #endregion
+        }
+    }
+}
