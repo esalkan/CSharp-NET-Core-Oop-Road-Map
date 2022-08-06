76994b8fdc141c1b01c9ce07b3fd90787a7f37c7
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-08-06 16:18:02 +0300

    Char Variable Type & Char Methods

---
+++ CS01_04_CharVariableTypeAndMethods/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CS01_04_CharVariableTypeAndMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Char Variable Type & Char Methods
+            // Genellikle değişkende tek bir karakter saklamak için kullanırız.
+            // Usually we use it to store a single character in the variable.
+
+            // Kullanıcı Seçimi : E
+            // User Select : E
+
+            char userSelectChar = 'A';
+
+            Console.WriteLine(userSelectChar);
+            #endregion
+        }
+    }
+}
