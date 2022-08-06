afc1d8da81479490be0d4b146cf00b71b9ea2b92
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-08-06 18:02:48 +0300

    DateTime

---
+++ CS01_08_DateTime/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CS01_08_DateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. User working hours 13:00 - 17:00
+            // 1. Kullanıcı çalışma saatleri 13:00 - 17:00
+            
+            // 2. User working hours 17:00 - 21:00
+            // 2. Kullanıcı working hours 17:00 - 21:00
+            
+            /*
+             * 1. User scan the card
+             *      User 1 scanned card inside id is 1
+             * 1. User came at 14:00, is user can pass in? = true
+             * 2. User came at 14:00, is user can pass in? = false
+             */
+            
+            Console.WriteLine($"DateTime.Now : {DateTime.Now}");
+            Console.WriteLine($"DateTime.Today : {DateTime.Today}");
+            Console.WriteLine($"DateTime.MinValue : {DateTime.MinValue}");
+            Console.WriteLine($"DateTime.MaxValue : {DateTime.MaxValue}");
+            Console.WriteLine($"DateTime.UtcNow : {DateTime.UtcNow}");
+        }
+    }
+}
+
