7808538b59a64c98c7b48b2ced300de5dc356db5
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-08-06 18:11:43 +0300

    Variable Behaviors

---
+++ CS01_09_VariableBehaviors/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CS01_09_VariableBehaviors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string val1 = "User 1";
+            string val2 = "User 2";
+            val1 = val2;
+            val2 = "user 3";
+        }
+    }
+}
