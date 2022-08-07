f8d474aa01d0a69553f2298b04b757c15dd5adab
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-08-07 13:32:27 +0300

    As Operator

---
+++ CS01_29_AsOperator/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CS01_29_AsOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * As Operator
+             * Elimizdeki herhangi bir nesneyi T tipine çevirebiliyorsa, çeviren, yok eğer çeviremiyorsa null dönen bir operatördür.
+             * If it can convert any object we have to the type T, it is an operator that converts, or if it cannot, it returns null.
+             */
+            object language = "C# Java Python";
+            object objectString = language;
+            string resultStr = objectString as string;
+            
+        }
+    }
+}
