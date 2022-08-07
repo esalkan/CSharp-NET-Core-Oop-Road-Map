215b823e077d12bd4995a1b0dec34cc9e5ad6984
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-08-07 11:33:09 +0300

    Arithmetic Operators Substraction

---
+++ CS01_20_ArithmeticOperators_Substract/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CS01_21_ArithmeticOperators_Substract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Çıkartma | Substraction (-) Operator
+             */
+
+            int num1 = 100;
+            int num2 = 50;
+            int resultInt = num1 - num2;
+            Console.WriteLine("Int substraction result is : " + resultInt);
+
+            byte byt1 = 100;
+            byte byt2 = 50;
+            var resultByte = byt1 - byt2;
+            Console.WriteLine("Byte substraction result is : " + resultByte);
+        }
+    }
+}
