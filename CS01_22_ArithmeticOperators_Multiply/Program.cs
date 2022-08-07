895b50f80e8f618d73fa953c9b0ab0f860194104
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-08-07 11:47:15 +0300

    Arithmetic Operators Multiply

---
+++ CS01_22_ArithmeticOperators_Multiply/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CS01_22_ArithmeticOperators_Multiply
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Multiply | Çarpma Operatörü
+             * Operantor = "*"
+             */
+
+            double d1 = 10.4;
+            double d2 = 10.6;
+            double resultD = d1 * d2;
+
+            // int resultInt = d1 * d2;
+            // int resultInt = (int) d1 * d2;
+            int resultInt = (int) (d1 * d2);
+            
+        }
+    }
+}
