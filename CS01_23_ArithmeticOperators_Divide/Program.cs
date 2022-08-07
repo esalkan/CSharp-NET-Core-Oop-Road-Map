b1ee7ddba0fd5209ddaf34787ef6f2fc98b4c191
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-08-07 11:52:12 +0300

    Arithmetic Operators Divide

---
+++ CS01_23_ArithmeticOperators_Divide/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CS01_23_ArithmeticOperators_Divide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Divide | Bölme
+             * Operator : "/"
+             */
+
+            int s1 = 10;
+            int s2 = 2;
+            int result1 = s1 / s2;
+
+            int s3 = 0;
+            int result2 = s1 / s3; // System.DivideByZeroException.
+
+        }
+    }
+}
+
