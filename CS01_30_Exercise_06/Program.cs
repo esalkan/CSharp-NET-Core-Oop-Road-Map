95f058b01b3bf616e3e26235aa053ec5893ba602
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-08-07 14:10:12 +0300

    Exercise 06

---
+++ CS01_30_Exercise_06/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace CS01_30_Exercise_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Kullanıcıdan gelen 3 farklı sınav notunun ortalaması 45 değerine eşit veya büyük mü kontrolünü sağlayın.
+             * Check if the average of 3 different exam grades from the user is equal to or greater than 45.
+             */
+            
+            string exam1, exam2, exam3;
+
+            Console.WriteLine("Exam 1 result : ");
+            exam1 = Console.ReadLine();
+            
+            Console.WriteLine("Exam 2 result : ");
+            exam2 = Console.ReadLine();
+            
+            Console.WriteLine("Exam 3 result : ");
+            exam3 = Console.ReadLine();
+
+            decimal examD1 = Convert.ToDecimal(exam1);
+            decimal examD2 = Convert.ToDecimal(exam2);
+            decimal examD3 = Convert.ToDecimal(exam3);
+
+            var avg = (examD1 + examD2 + examD3) / 3;
+            bool result = avg >= 45;
+            Console.WriteLine($"Average Value is : {result}");
+
+            Console.WriteLine("--------------- o ---------------");
+            
+            /*
+             * UserName : admin
+             * Password : 123
+             */
+            
+            string userName, password;
+
+            Console.WriteLine("User Name : ");
+            userName = Console.ReadLine();
+            
+            Console.WriteLine("Password : ");
+            password = Console.ReadLine();
+
+            var userNameControl = userName is "admin";
+            var passwordControl = password is "123";
+
+            Console.WriteLine($"Given UserName is {userNameControl}\nGiven Password is {true}");
+        }
+    }
+}
