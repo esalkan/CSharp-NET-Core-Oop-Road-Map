0f6d06a9dbe86230b5bf5d0ab437c1bc551189d2
Eyüp Sabri ALKAN <eyup.alkan@medirect.com.mt>
2022-08-10 23:39:50 +0300

    Control Flow Statement / IF-ELSE Statement

---
+++ CS01_32_ControlFlowStatement_IF-ELSE/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CS01_32_ControlFlowStatement_IF_ELSE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // IF-ELSE Statements
+
+            int score = 60;
+
+            if (score >= 70)
+            {
+                Console.WriteLine("Excelent");
+                Console.WriteLine("You Passed With Grade A");
+            }
+            else
+            {
+                Console.WriteLine("Your Score is  : " + score);
+                Console.WriteLine("You fail");
+            }
+        }
+    }
+}
