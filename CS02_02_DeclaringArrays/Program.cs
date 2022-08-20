27686b03a59e10e84761aacff16479c8b315025b
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-08-20 17:07:59 +0300

    Arrays - Declaring Arrays

---
+++ CS02_02_DeclaringArrays/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CS02_02_DeclaringArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Declaring Arrays
+             *
+             * To declare an array in C#, we can use the following syntax
+             *      dataType[] arrayName;
+             *
+             * dataType is used to specify the type of elements in the array
+             * [] specifies the rank of the array. The rank specifies the size of the array
+             * arrayName specifies the name of the array.
+             *
+             * C# ile bir dizi bildirmek için aşağıdaki sözdizimini kullanabiliriz.
+             *      dataType[] diziAdı;
+             *
+             * dataType, dizideki öğelerin türünü belirtmek için kullanılır.
+             * [] dizinin sırasını belirtir. Rank, dizinin boyutunu belirtir.
+             * arrayName dizinin adını belirtir.
+             */
+
+            double[] balance;
+        }
+    }
+}
