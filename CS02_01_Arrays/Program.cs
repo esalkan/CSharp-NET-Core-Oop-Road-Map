485331b487615046516c6700d9467a9a4eeb2f43
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-08-20 17:02:57 +0300

    Arrays

---
+++ CS02_01_Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CS02_01Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * An array stores a fixed-size sequential collection of elements of the same type.
+             * An array is used to store a collection of data, but it is often more useful to think of an array
+             * as a collection of variables of the same type stored at contiguous memory locations.
+             *
+             * Instead of declaring individual variables, such as number0, number1, ..., and number99,
+             * you declare one array variable such as numbers and use numbers[0], numbers[1], and ..., numbers[99]
+             * to represent individual variables. A specific element in an array is accessed by an index.
+             *
+             * Bir dizi, aynı türdeki öğelerin sabit boyutlu sıralı bir koleksiyonunu depolar. Bir dizi,
+             * bir veri koleksiyonunu depolamak için kullanılır, ancak bir diziyi, bitişik bellek konumlarında
+             * depolanan aynı türdeki değişkenlerin bir koleksiyonu olarak düşünmek genellikle daha yararlıdır.
+             *
+             * sayı0, sayı1, ... ve sayı99 gibi tek tek değişkenleri bildirmek yerine, sayılar gibi bir dizi
+             * değişkeni bildirir ve bağımsız değişkenleri temsil etmek için sayıları[0], numbers[1], ve ...,numbers[99]
+             * kullanırsınız.Dizideki belirli bir öğeye bir dizin tarafından erişilir.
+             */
+        }
+    }
+}
