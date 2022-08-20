152109d13f89825de995461209ce43ce9a967b97
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-08-20 17:11:58 +0300

    Arrays - Initializing an Array

---
+++ CS02_03_InitializingArray/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace CS02_03_InitializingArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Initializing an Array
+             * Declaring an array does not initialize the array in the memory.
+             * When the array variable is initialized, we can assign values to the array.
+             *
+             * Array is a reference type, so we need to use the new keyword to create an instance of the array.
+             *
+             * Bir Diziyi Başlatma
+             * Bir dizi bildirmek, diziyi bellekte başlatmaz.
+             * Dizi değişkeni başlatıldığında diziye değerler atayabiliriz.
+             *
+             * Dizi bir referans türüdür, bu nedenle dizinin bir örneğini oluşturmak için new anahtar sözcüğünü kullanmamız gerekir.
+             */
+
+            double[] balance = new double[10];
+        }
+    }
+}
