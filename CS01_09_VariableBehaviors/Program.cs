1fb71a0ccd33cdc6329338e8e3612d24146fc5d0
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-08-20 22:22:01 +0300

    Variable Behaviors

--- CS01_09_VariableBehaviors/Program.cs
+++ CS01_09_VariableBehaviors/Program.cs
@@ -1,12 +1,19 @@
-﻿namespace CS01_09_VariableBehaviors;
-
-internal class Program
+﻿namespace CS01_09_VariableBehaviors
 {
-    private static void Main(string[] args)
+    class Program
     {
-        var val1 = "User 1";
-        var val2 = "User 2";
-        val1 = val2;
-        val2 = "user 3";
+        static void Main(string[] args)
+        {
+            var val1 = "User 1";
+            Console.WriteLine(val1);
+            var val2 = "User 2";
+            Console.WriteLine(val2);
+            val1 = val2;
+            val2 = "user 3";
+            
+            
+            Console.WriteLine(val1);
+            Console.WriteLine(val2);
+        }
     }
 }
