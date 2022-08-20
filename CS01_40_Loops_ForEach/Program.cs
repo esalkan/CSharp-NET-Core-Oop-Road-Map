a18cf02f9d1e7c5a02c6c1a84a05c1696b071a6b
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-08-20 11:27:11 +0300

    Foreach loop

---
+++ CS01_40_Loops_ForEach/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CS01_40_Loops_ForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region General Usage | Genel Kullanım
+            string[] fruits = { "Watermelon", "Apple", "Banana", "Grape", "Coconat" };
+
+            foreach (var fruit in fruits)
+            {
+                Console.WriteLine(fruit);
+            }
+            #endregion
+
+            #region Examples | Örnekler
+
+            
+
+            #endregion
+        }
+    }
+}
