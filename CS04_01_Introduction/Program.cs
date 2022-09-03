e1716e47b5d17712853ab86576a289cb714d771d
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-09-03 16:33:36 +0300

    Method - Introduction

---
+++ CS04_01_Introduction/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace CS04_01_Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * METHODS
+             *
+             * A method is a group of statements that together perform a task.
+             * Every C# program has at least one class with a method named Main.
+             *
+             *  To use a method, we need to;
+             *      − Define the method
+             *      - Call the method
+             *
+             *
+             * Defining Methods in C#
+             *  When we define a method, we basically declare the elements of its structure. The syntax for defining
+             *  a method in C# is as follows;
+             *      <Access Specifier> <Return Type> <Method Name> (Parameter List){
+             *          Method Body
+             *      }
+             *
+             * Following are the various elements of a method;
+             *  - Access Specifier;
+             *      This determines the visibility of a variable or a method from another class.
+             *
+             *  - Return Type;
+             *      A method may return a value. The return type is the data type of the value the method returns.
+             *      If the method is not returning any values, then the return type is "void"
+             *
+             *  - Method Name;
+             *      Method name is a unique identifier and it is case sensitive. It cannot be same as any other
+             *      identifier declared in the class.
+             *
+             *  - Parameter list;
+             *      Enclosed between parentheses, the parameters are used to pass and receive data from a method.
+             *      The parameter list refers to the type, order, and number of the parameters of a method. Parameters
+             *      are optional; that is, a method may contain no parameters.
+             *
+             *  - Method Body;
+             *      This contains the set of instructions needed to complete the required activity.
+             *
+             * ---------------------------------------------------------------------------------------------------------
+             *
+             * Methods
+             *      Yöntem(Method), birlikte bir görevi yerine getiren bir grup ifadedir.
+             *      Her C# programında Main adında bir metoda sahip en az bir sınıf bulunur.
+             *
+             * Bir yöntemi(method) kullanmak için yapmamız gerekenler -
+             *      - Method'u Tanımlayın
+             *      - Method'u Çağırın
+             *
+             * C# ile Metot Tanımlama
+             *      Bir metot tanımladığımızda, temelde onun yapısının elemanlarını beyan etmiş olursunuz.
+             *      C#'ta bir yöntem(metot) tanımlamanın sözdizimi aşağıdaki gibidir -
+             *
+             *          <Erişim Tanımlayıcı> <Dönüş Tipi> <Metot Adı>(Parametre Listesi) {
+             *              Metot Gövdesi
+             *          }
+             *
+             * Bir metodun çeşitli unsurları aşağıdaki gibidir -
+             *  - Erişim Tanımlayıcı;
+             *          Bu, bir değişkenin veya başka bir sınıftan bir metodn görünürlüğünü belirler.
+             *
+             *  - Dönüş Tipi;
+             *          Bir metot bir değer döndürebilir. Dönüş türü, metodun döndürdüğü değerin veri türüdür.
+             *          Metod herhangi bir değer döndürmüyorsa, dönüş türü "void" dir.
+             *
+             *  - Method Name;
+             *          Metod adı benzersiz bir tanımlayıcıdır ve büyük/küçük harf duyarlıdır.
+             *          Sınıfta(Class) bildirilen diğer tanımlayıcılarla aynı olamaz.
+             *
+             *  - Parametre Listesi;
+             *          Parantez içine alınmış parametreler, bir metoda veri göndermek ve metotdan veri almak için
+             *          kullanılır. Parametre listesi, bir metodun parametrelerinin tipini, sırasını ve sayısını ifade
+             *          eder. Parametreler isteğe bağlıdır; yani, bir metod hiç bir parametre almayabilir/içermeyebilir.
+             *
+             *  - Metot Gövdesi;
+             *          Gerekli/Yapılması gereken  iş/aktiviteyi tamamlamak için gereken talimat setini içerir.
+             *      
+             */
+        }
+    }
+}
