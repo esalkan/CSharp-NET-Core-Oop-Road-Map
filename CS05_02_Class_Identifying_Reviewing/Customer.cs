7ffd3183290aebd0aa17c35cbf72a65322f31252
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-09-04 17:04:58 +0300

    OOP - Class Identifying and Reviewing

---
+++ CS05_02_Class_Identifying_Reviewing/Customer.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS05_02_Class_Identifying_Reviewing;
+
+public class Customer
+{
+    public string TcID;
+    public string name;
+    public string surname;
+    public int gender;
+    
+    
+}
---
+++ CS05_02_Class_Identifying_Reviewing/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CS05_02_Class_Identifying_Reviewing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating a new Customer
+            Customer c1 = new Customer();
+            
+            // Assigning values to the c1 Customer
+            c1.TcID = "98765432109";
+            c1.name = "Mete";
+            c1.surname = "HAN";
+            c1.gender = 717770001;
+
+            Console.Clear();
+            Console.WriteLine("Customer informations");
+            Console.WriteLine($"ID : {c1.TcID}\nName : {c1.name}\nSurname : {c1.surname}\nGender : {c1.gender}");
+        }
+    }
+}
