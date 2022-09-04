aaa50a296b0748b4ca277ea188e88ca565ecde83
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-09-04 15:13:32 +0300

    Methods - Local Function

---
+++ CS04_10_Local_Functions/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CS04_10_Local_Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LocalFunctionUsage(2,2);
+        }
+
+        static void LocalFunctionUsage(int num1, int num2)
+        {
+            // Local Functions usage came with .NET 7
+            int sum(int incomingNum1, int incomingNum2)
+            {
+                return incomingNum1 + incomingNum2;
+            }
+
+            int totalValue = sum(num1, num2);
+
+            Console.WriteLine($"Total Value is : {totalValue}");
+        }
+    }
+}
