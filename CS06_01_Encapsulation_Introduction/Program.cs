46508799b4cb0c48124f50248fd2dcdc40a55d62
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-09-11 17:37:57 +0300

    OOP - Encapsulation | Introduction

---
+++ CS06_01_Encapsulation_Introduction/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace CS06_01_Encapsulation_Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * ENCAPSULATION INTRODUCTION
+             *
+             * Encapsulation is defined 'as the process of enclosing one or more items within a physical or logical
+             * package'. Encapsulation, in object oriented programming methodology, prevents access to implementation
+             * details.
+             * 
+             * Encapsulation is implemented by using access specifiers. An access specifier defines the scope and
+             * visibility of a class member. C# supports the following access specifiers −
+             *      - Public
+             *      - Private
+             *      - Protected
+             *      - Internal
+             *      - Protected internal
+             *
+             * - Public Access Specifier
+             * Public access specifier allows a class to expose its member variables and member functions to other
+             * functions and objects. Any public member can be accessed from outside the class.
+             *
+             * - Private Access Specifier
+             * Private access specifier allows a class to hide its member variables and member functions from other
+             * functions and objects. Only functions of the same class can access its private members.
+             * Even an instance of a class cannot access its private members.
+             *
+             * - Protected Access Specifier
+             * Protected access specifier allows a child class to access the member variables and member functions of
+             * its base class. This way it helps in implementing inheritance. We will discuss this in more details in
+             * the inheritance chapter.
+             *
+             * - Internal Access Specifier
+             * Internal access specifier allows a class to expose its member variables and member functions to other
+             * functions and objects in the current assembly. In other words, any member with internal access specifier
+             * can be accessed from any class or method defined within the application in which the member is defined.
+             *
+             * - Protected Internal Access Specifier
+             * The protected internal access specifier allows a class to hide its member variables and member functions from other
+             * class objects and functions, except a child class within the same application. This is also used while implementing
+             * inheritance.
+             *
+             * ---------------------------------------------------------------------------------------------------------
+             * KAPSÜLLEME - ENCAPSULATION
+             * 
+             * Kapsülleme, 'bir veya daha fazla öğeyi fiziksel veya mantıksal bir paket içine alma süreci'
+             * olarak tanımlanır.Kapsülleme, nesne yönelimli programlama metodolojisinde, uygulama detaylarına erişimi
+             * engeller.
+             *
+             * Kapsülleme, erişim belirteçleri kullanılarak gerçekleştirilir. Bir erişim belirteci,
+             * bir sınıf üyesinin kapsamını ve görünürlüğünü tanımlar. C# aşağıdaki erişim belirteçlerini destekler -
+             *      - Public
+             *      - Private
+             *      - Protected
+             *      - Internal
+             *      - Protected internal
+             *
+             * - (Public) Genel Erişim Belirteci
+             * Genel erişim belirteci, bir sınıfın üye değişkenlerini ve üye işlevlerini diğer işlevlere ve nesnelere
+             * göstermesine izin verir. Herhangi bir genel üyeye sınıf dışından erişilebilir.
+             *
+             * - (Private) Özel Erişim Belirteci
+             * Özel erişim belirteci, bir sınıfın üye değişkenlerini ve üye işlevlerini diğer fonksiyonlardan ve nesnelerden
+             * gizlemesine izin verir. Yalnızca aynı sınıfın işlevleri, özel üyelerine erişebilir.
+             * Bir sınıfın bir örneği bile onun özel üyelerine erişemez.
+             *
+             * - (Protected) Korumalı Erişim Belirteci
+             * Korumalı erişim belirteci, bir alt sınıfın, temel sınıfının üye değişkenlerine ve üye işlevlerine
+             * erişmesine izin verir. Bu şekilde mirasın(inheritance) uygulanmasına yardımcı olur.
+             * Bunu miras(inheritance) bölümünde daha ayrıntılı olarak ele alınacaktır.
+             *
+             * - (Internal) Dahili Erişim Belirteci
+             * Dahili erişim belirteci, bir sınıfın üye değişkenlerini ve üye işlevlerini geçerli derlemedeki
+             * diğer işlevlere ve nesnelere göstermesine izin verir. Başka bir deyişle, dahili erişim belirtecine sahip
+             * herhangi bir üyeye, üyenin tanımlandığı uygulama içinde tanımlanan herhangi bir sınıf veya yöntemden
+             * erişilebilir.
+             * 
+             */
+        }
+    }
+}
