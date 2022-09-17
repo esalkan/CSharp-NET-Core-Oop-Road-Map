9383fb4569b5e3447969605af3d53c0996ef60ca
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-09-17 13:26:08 +0300

    OOP - Static - Static Class Usage

---
+++ CS07_05_Static_Class_Usage/Helper.cs
@@ -0,0 +1,54 @@
+namespace CS07_05_Static_Class_Usage;
+
+// Static Classes can not be instantiated. Helper H1 = new Helper(); cant use.
+// Static Classlar örneklenemez. Helper H1 = new Helper(); kullanılamaz.
+public static class Helper
+{
+    // -----------------------------------------------------------------------------------------------------------------
+
+    // NON-STATIC FIELD/PROPERTY CANNOT BE CONTAINED IN STATIC CLASSES.
+    // STATIC CLASSLARIN ICERISINDE STATIC OLMAYAN ALANLAR/PROPERTY BULUNAMAZ.
+    // public string Subject { get; set; }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    // NON-STATIC METHODS CANNOT BE CONTAINED STATIC CLASSES.
+    // STATIC CLASSLARIN ICERISINDE STATIC OLMAYAN METODLAR BULUNAMAZ
+    // public void Test()
+    // {
+    //    Console.WriteLine("Test 1");
+    // }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    
+    // CONSTRUCTOR METHOD
+    
+    // NON-STATIC CONSTRUCTORS CAN NOT BE CONTAINED IN STATIC CLASSES.
+    // STATIC OLMAYAN CONSTRUCTORLAR STATIC CLASS ICERISINDE YER ALAMAZLAR
+    // public Helper() {} // Her nesne örneği oluşturulduğunda uygulama standart yapıcı/constructor metodu çağırır
+                          // The application calls the standard constructor method every time an object instance is created
+    static Helper()
+    {
+        // STATIC CONSTRUCTOR
+        // Uygulama içerisinde istenildiği kadar çağırılsada sadece ilk çağırılmada çalışırlar...
+        // Even if they are called as many times as desired in the application, they only work on the first call...
+        Console.WriteLine("Helper.cs => Static Class => Static Constructor");
+        Console.WriteLine();
+    }
+    
+    // Static Method for send an Email
+    public static void sendEmail(string recipientEmail, string subject, string content)
+    {
+        // Email send process will contuniue....
+        // Email gönderme işlemleri devam edecek...
+        Console.WriteLine("Mail Sent!");
+        Console.WriteLine("Mail Gönderildi!");
+        Console.WriteLine();
+    }
+
+    
+
+
+    
+}
---
+++ CS07_05_Static_Class_Usage/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace CS07_05_Static_Class_Usage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            
+            Employee employee1 = new Employee();
+            employee1.Name = "Badi";
+            employee1.LastName = "Ekrem";
+            employee1.EmailAddress = "badi.ekrem";
+            employee1.Gender = 717770001;
+            
+            // Helper Class created.
+            // sendEmail static method created and used.
+            Helper.sendEmail("hr@company.com","New Employee",employee1.Name + employee1.LastName);
+            
+            // Static Class Usage
+            Helper.sendEmail("hr@company.com","New Employee",employee1.Name + employee1.LastName);
+            /*
+             * Static Constructor will run first, as we can see above we call static method 2 times.
+             * And we will see just one console output.
+             *
+             * Önce Static Constructor çalışacaktır, yukarıda gördüğümüz gibi static methodu 2 defa çağırıyoruz.
+             * Ve sadece bir konsol çıktısı göreceğiz.
+             *
+             * Console Output/Çıktı:
+             *      Helper.cs => Static Class => Static Constructor
+             */
+        }
+    }
+}
