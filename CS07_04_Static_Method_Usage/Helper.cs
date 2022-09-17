773d0825c5259795c68af45ef13d81e3f61b3b72
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-09-17 13:07:18 +0300

    OOP - Static - Method Usage

--- CS07_04_Static_Method_Usage/Helper.cs
+++ CS07_04_Static_Method_Usage/Helper.cs
@@ -1,6 +1,8 @@
 namespace CS07_04_Static_Method_Usage;
 
-public class Helper
+// Static Classes can not be instantiated. Helper H1 = new Helper(); cant use.
+// Static Classlar örneklenemez. Helper H1 = new Helper(); kullanılamaz.
+public static class Helper
 {
     // Static Method for send an Email
     public static void sendEmail(string recipientEmail, string subject, string content)
