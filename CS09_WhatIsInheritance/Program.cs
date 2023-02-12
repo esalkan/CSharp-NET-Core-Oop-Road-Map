f8035d7c0e30c246d0ead036d39a6d88c01f3373
Eyüp Sabri ALKAN <eyup.alkan@medirect.com.mt>
2023-02-12 22:08:46 +0300

    What is inheritance

---
+++ CS09_WhatIsInheritance/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS09_WhatIsInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student S1 = new Student();
+            // S1.
+        }
+    }
+}
---
+++ CS09_WhatIsInheritance/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS09_WhatIsInheritance
+{
+    public class Student
+    {
+        #region Fiels
+        public int studentId { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        #endregion
+    }
+}
