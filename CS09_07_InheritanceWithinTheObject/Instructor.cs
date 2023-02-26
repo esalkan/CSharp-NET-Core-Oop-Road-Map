cbd4073228eb61c7efa3caaee2dc360a4b256aac
Eyüp Sabri ALKAN <eyup.alkan@medirect.com.mt>
2023-02-26 09:05:26 +0300

    Inheritance Within The Object

---
+++ CS09_07_InheritanceWithinTheObject/Instructor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS09_07_InheritanceWithinTheObject
+{
+    // Sealed Class
+    // This object is public and can be accessed externally.
+    // But sealed and this object cannot be instantiated.
+    public sealed class Instructor : Staff
+    {
+        #region Fields/Properties | Alanlar/Özellikler
+        public string Branch { get; set; }
+        public int BranchLevel { get; set; }
+        #endregion
+
+        #region Constructor Method | Özel Metodlar
+        public Instructor()
+        {
+            Console.WriteLine("Instructor Object Constructor method is RUN.");
+        }
+        #endregion
+    }
+}
---
+++ CS09_07_InheritanceWithinTheObject/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CS09_07_InheritanceWithinTheObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Instructor I1 = new Instructor();
+
+            // When we inherit objects, their behavior is like below example. (Please check related classes)
+
+            // First Run:   Step by step
+            //              Instructor => Staff => BaseType => Object
+            //
+            // Returning Back:  Step by step
+            //                  Object => BaseType => Staff => Instructor
+        }
+    }
+}
