adeb08f4dd21a16713e7ba1a93caf5dc6e51dcf1
Eyüp Sabri ALKAN <eyup.alkan@medirect.com.mt>
2023-03-12 22:18:55 +0300

    Virtual Method

---
+++ CS10_01_VirtualMethod/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS10_01_VirtualMethod
+{
+    public class Customer // Object => Framework standart olarak tüm nesneleri object'den üretir.
+                          // Object => Framework generates all objects from object as standard.
+    {
+        public int customerID { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+
+        // Virtual Method
+        // Manipuating built-in ToString() Method
+        public override string ToString()
+        {
+            return customerID + " " + name + " " + surname;
+        }
+    }
+}
---
+++ CS10_01_VirtualMethod/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+
+namespace CS10_01_VirtualMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /**
+             * In C#, a virtual method is a method that can be overridden in a derived class.
+             * When a method is declared as virtual in a base class, it allows a derived class to
+             * provide its own implementation of the method.
+             * 
+             * To declare a method as virtual in C#, the "virtual" keyword is used in the method 
+             * declaration in the base class.
+             * 
+             * ---------------------------------------------------------------------------------
+             * 
+             * C#'ta sanal bir yöntem, türetilmiş bir sınıfta geçersiz kılınabilen bir yöntemdir 
+             * Bir yöntem, bir temel sınıfta sanal olarak bildirildiğinde, türetilmiş bir 
+             * sınıfın, yöntemin kendi uygulamasını sağlamasına izin verir.
+             * 
+             * C#'ta bir yöntemi sanal olarak bildirmek için, temel sınıftaki yöntem bildiriminde
+             * "virtual" anahtar sözcüğü kullanılır.
+            */
+
+            // Object => ToString();
+
+            /*
+             * Customer C1 = new Customer();
+             * 
+             * Object => Customer
+             *           C1.ToString();
+             *           
+             * Namespace.Customer
+             * 
+             * 
+             */
+
+            // Let's take an object instance from the customer object.
+            // Müşteri nesnesinden bir nesne örneği alalım.
+            Customer C1 = new  Customer();
+            C1.customerID = 1;
+            C1.name = "Nasreddin";
+            C1.surname = "HOCA";
+
+            string toStringMessage = C1.ToString();
+
+            Console.WriteLine(toStringMessage);
+        }
+    }
+}
