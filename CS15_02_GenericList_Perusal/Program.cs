7832e224da1c1d504197aecb0c1c697a9259fb53
Eyup ALKAN <esalkan@icloud.com>
2023-04-23 21:00:20 +0300

    Insert(), ForEach(), Any(), Sort(), Reverse(), Remove(), RemoveAll(), RemoveAt(), Clear() Methods in List<T>

--- CS15_02_GenericList_Perusal/Program.cs
+++ CS15_02_GenericList_Perusal/Program.cs
@@ -41,7 +41,9 @@ namespace CS15_02_GenericList_Perusal
                 Console.WriteLine(i);
             }
 
+            Console.WriteLine("---------------------------------------------------------");
             // ---------------------------------------------------------------------------------
+
             // Capacity
             // Count
 
@@ -56,11 +58,106 @@ namespace CS15_02_GenericList_Perusal
             capacity = myNumbers.Capacity;
             count = myNumbers.Count;
 
+            Console.WriteLine("---------------------------------------------------------");
             // ---------------------------------------------------------------------------------
+
             // ForEach()
             // => lambda, ilgili koleksiyon içerisinde arama yap ve konsola yazdır. | => List<T> içerisinde Linq konusudur.
             // => lambda, search the related collection and print it to the console.| => is the subject of Linq in List<T>.
             myNumbers.ForEach(item => Console.WriteLine(item));
+            
+            Console.WriteLine("---------------------------------------------------------");
+            // ---------------------------------------------------------------------------------
+            
+            // İşaret edilen index'te olan değeri alma.
+            // Get the value at the pointed index.
+            int containedValue = myNumbers[3];
+            Console.WriteLine("List<T> koleksiyonu içerisinde 3. index'de bulunan değer {0}", containedValue);
+            Console.WriteLine("The value at index 3 in the List<T> collection is {0}", containedValue);
+            
+            Console.WriteLine("---------------------------------------------------------");
+            // ---------------------------------------------------------------------------------
+
+            // Insert() Method
+            // ForEach() Method
+
+            // The Insert method is used to insert an item into the middle of the list.
+            // The item inserted is a duplicate, which is later removed using the Remove method.
+
+            // Insert yöntemi, listenin ortasına bir öğe eklemek için kullanılır.
+            // Eklenen öğe bir kopyadır ve daha sonra Remove yöntemi kullanılarak kaldırılır
+            Console.WriteLine("Insert() & ForEach()");
+            myNumbers.Insert(3, 100);
+            myNumbers.ForEach(item => Console.WriteLine(item));
+
+            Console.WriteLine("---------------------------------------------------------");
+            // ---------------------------------------------------------------------------------
+
+            // Any() Method
+            Console.WriteLine("Any()");
+            bool control1 = myNumbers.Any();
+            bool control2 = myNumbers.Any(item => item > 5);
+
+            Console.WriteLine("---------------------------------------------------------");
+            // ---------------------------------------------------------------------------------
+
+            // Sort() Method 
+
+            // Sorts the elements or a portion of the elements in the List<T> using either the specified or
+            // default IComparer<T> implementation or a provided Comparison<T> delegate to compare list elements.
+
+            // Liste öğelerini karşılaştırmak için belirtilen veya varsayılan IComparer<T> uygulamasını ya da
+            // sağlanan bir Comparison<T> temsilcisini kullanarak List<T> içindeki öğeleri veya öğelerin bir kısmını sıralar.
+            Console.WriteLine("Sort()");
+            myNumbers.Sort(); // A-Z | 1 - N..
+
+            Console.WriteLine("---------------------------------------------------------");
+            // ---------------------------------------------------------------------------------
+
+            // Reverse() Method
+
+            // Reverses the order of the elements in the List<T> or a portion of it.
+            // List<T>'deki veya bir bölümündeki öğelerin sırasını tersine çevirir.
+            Console.WriteLine("Reverse()");
+            myNumbers.Reverse(); // Z-A | N...- 1
+
+            Console.WriteLine("---------------------------------------------------------");
+            // ---------------------------------------------------------------------------------
+
+            // Remove() Method
+            // Removes the first occurrence of a specific object from the List<T>.
+            // List<T> listesinden belirli bir nesnenin ilk geçtiği yeri kaldırır.
+            Console.WriteLine("Remove()");
+            bool removeResult = myNumbers.Remove(100);
+
+            Console.WriteLine("---------------------------------------------------------");
+            // ---------------------------------------------------------------------------------
+
+            // RemoveAll()
+            // Removes all the elements that match the conditions defined by the specified predicate.
+            // Belirtilen yüklem tarafından tanımlanan koşullarla eşleşen tüm öğeleri kaldırır.
+            // myNumbers.RemoveAll(item => item > 3);
+            Console.WriteLine("RemoveAll()");
+            int deletedQuantity = myNumbers.RemoveAll(item => item > 3);
+
+            Console.WriteLine("---------------------------------------------------------");
+            // ---------------------------------------------------------------------------------
+
+            // RemoveAt() Method
+            // Removes the element at the specified index of the List<T>.
+            // List<T>'nin belirtilen dizinindeki öğeyi kaldırır.
+            Console.WriteLine("RemoveAt()");
+            myNumbers.RemoveAt(2); // That will removed index 2 value | Index 2'de bulunan değeri kaldırır.
+
+            Console.WriteLine("---------------------------------------------------------");
+            // ---------------------------------------------------------------------------------
+
+            // Clear() Method
+            // Removes all elements from the List<T>.
+            // List<T>'den tüm öğeleri kaldırır.
+            Console.WriteLine("Clear()");
+            myNumbers.Clear();
+
         }
     }
 }
