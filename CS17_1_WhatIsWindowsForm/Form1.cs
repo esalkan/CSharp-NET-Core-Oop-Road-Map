65829b9016bf5c466462c41cb68d7242cb59886d
Eyup ALKAN <esalkan@icloud.com>
2023-07-16 17:19:58 +0300

    Windows Form

---
+++ CS17_1_WhatIsWindowsForm/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS17_1_WhatIsWindowsForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openSecondFormButton_Click(object sender, EventArgs e)
+        {
+            HelloWindowsForm helloWindowsForm = new HelloWindowsForm();
+            helloWindowsForm.Show();
+        }
+    }
+}
---
+++ CS17_1_WhatIsWindowsForm/HelloWindowsForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS17_1_WhatIsWindowsForm
+{
+    public partial class HelloWindowsForm : Form
+    {
+        public HelloWindowsForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
