e548131e84b8ff2189742633a154971fd41af582
Eyup ALKAN <esalkan@icloud.com>
2023-07-17 07:34:02 +0300

    System.IO » Directory

--- CS18_01_SystemIO_Directory_Operations/Program.cs
+++ CS18_01_SystemIO_Directory_Operations/Program.cs
@@ -13,11 +13,15 @@ namespace CS18_01_SystemIO_Directory_Operations
         {
             // Yeni klasör oluşturma.
             // Creating new folder.
-            //CreateNewFolder("C:\\MePlayGround\\GitHubRepos\\CSharp\\Directory");
-            
+            CreateNewFolder("C:\\MePlayGround\\GitHubRepos\\CSharp\\Directory");
+
             // Klasör varlık kontrolü. (Var olup olmadığını kontrol ediyoruz.)
             // The folder existance control.
             CheckFolderExistance("C:\\MePlayGround\\GitHubRepos\\CSharp\\Directory");
+
+            // Klasör silme işlemi.
+            // Delete folder operation.
+            DeleteFolder("C:\\MePlayGround\\GitHubRepos\\CSharp\\Directory");
         }
 
         // Yeni klasör oluştur metodu.
@@ -29,10 +33,24 @@ namespace CS18_01_SystemIO_Directory_Operations
             DirectoryInfo DI= Directory.CreateDirectory(path);
         }
 
-
+        // Klasör varlık kontrolü. (Var olup olmadığını kontrol ediyoruz.)
+        // The folder existance control.
         static void CheckFolderExistance(string path)
         {
             bool control = Directory.Exists(path);
+        }
+
+        // Yolu verilen klasörün silme işlemini gerçekleştirir.
+        // Deletes the folder to which the path is given.
+        static void DeleteFolder(string path)
+        {
+            // İçerisinde dosya OLMAYAN bir klasörü siler.
+            // Deletes the folder wich is NOT HAVE any files.
+            Directory.Delete(path);
+
+            // İçerisinde dosya OLAN bir klasörü siler.
+            // Deletes the folder wich is HAVE any files.
+            // Directory.Delete(path, true);
         }
     }
 }
