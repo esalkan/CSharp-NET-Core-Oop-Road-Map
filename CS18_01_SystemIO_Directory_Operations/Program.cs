de9540f0733cbb54e4c0c2301561ad89b332af81
Eyup ALKAN <esalkan@icloud.com>
2023-07-17 07:13:07 +0300

    System.IO
    Directory class.
    » The Directory class actually allows us to perform folder operations on the hard disk.
    » Directory sınıfı bizim aslında sabit disk üzerinde klasör işlemleri yapmamıza olanak sağlar.

---
+++ CS18_01_SystemIO_Directory_Operations/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS18_01_SystemIO_Directory_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CreateNewFolder("C:\\MePlayGround\\GitHubRepos\\CSharp\\Directory");
+        }
+
+        static void CreateNewFolder(string path)
+        {
+            // The Directory class actually allows us to perform folder operations on the hard disk.
+            // Directory sınıfı bizim aslında sabit disk üzerinde klasör işlemleri yapmamıza olanak sağlar.
+            DirectoryInfo DI= Directory.CreateDirectory(path);
+        }
+    }
+}
