fbd7ddb7f42a52839f156452cf6dd75fee9ab976
Eyup ALKAN <esalkan@icloud.com>
2023-07-17 20:30:31 +0300

    System.IO
    Directory Create, Check, Move, Delete.

--- CS18_01_SystemIO_Directory_Operations/Program.cs
+++ CS18_01_SystemIO_Directory_Operations/Program.cs
@@ -11,22 +11,29 @@ namespace CS18_01_SystemIO_Directory_Operations
     {
         static void Main(string[] args)
         {
+            string path = @"C:\MePlayGround\GitHubRepos\CSharp\Directory";
+            string destinationPath = @"C:\MePlayGround\Directory";
             // Yeni klasör oluşturma.
             // Creating new folder.
-            CreateNewFolder("C:\\MePlayGround\\GitHubRepos\\CSharp\\Directory");
+            Create_Directory(path);
 
             // Klasör varlık kontrolü. (Var olup olmadığını kontrol ediyoruz.)
             // The folder existance control.
-            CheckFolderExistance("C:\\MePlayGround\\GitHubRepos\\CSharp\\Directory");
+            Check_Directory(path);
 
             // Klasör silme işlemi.
             // Delete folder operation.
-            DeleteFolder("C:\\MePlayGround\\GitHubRepos\\CSharp\\Directory");
+            Delete_Directory(path);
+
+            // Klasör taşıma
+            // Move Directory
+            Create_Directory(path);
+            Move_Directory(path, destinationPath);
         }
 
         // Yeni klasör oluştur metodu.
         // Create new folder method.
-        static void CreateNewFolder(string path)
+        static void Create_Directory(string path)
         {
             // The Directory class actually allows us to perform folder operations on the hard disk.
             // Directory sınıfı bizim aslında sabit disk üzerinde klasör işlemleri yapmamıza olanak sağlar.
@@ -35,14 +42,14 @@ namespace CS18_01_SystemIO_Directory_Operations
 
         // Klasör varlık kontrolü. (Var olup olmadığını kontrol ediyoruz.)
         // The folder existance control.
-        static void CheckFolderExistance(string path)
+        static void Check_Directory(string path)
         {
             bool control = Directory.Exists(path);
         }
 
         // Yolu verilen klasörün silme işlemini gerçekleştirir.
         // Deletes the folder to which the path is given.
-        static void DeleteFolder(string path)
+        static void Delete_Directory(string path)
         {
             // İçerisinde dosya OLMAYAN bir klasörü siler.
             // Deletes the folder wich is NOT HAVE any files.
@@ -52,5 +59,13 @@ namespace CS18_01_SystemIO_Directory_Operations
             // Deletes the folder wich is HAVE any files.
             // Directory.Delete(path, true);
         }
+
+        // Yolu verilen var olan klasörü, yolu verilen hedef noktaya taşır.
+        // Moves an existing folder to the destination point given the path.
+        static void Move_Directory(string path, string destinationPath)
+        {
+            Directory.Move(path,destinationPath);
+        }
+
     }
 }
