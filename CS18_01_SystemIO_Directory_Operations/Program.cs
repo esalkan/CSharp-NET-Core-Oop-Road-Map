5e9aa57c8ceca163182084bd90e969ab1e38f5c7
Eyup ALKAN <esalkan@icloud.com>
2023-07-17 07:21:45 +0300

    System.IO » Directory Class
    » Klasör varlık kontrolü. (Var olup olmadığını kontrol etme.)
    » The folder existance control.

--- CS18_01_SystemIO_Directory_Operations/Program.cs
+++ CS18_01_SystemIO_Directory_Operations/Program.cs
@@ -11,15 +11,28 @@ namespace CS18_01_SystemIO_Directory_Operations
     {
         static void Main(string[] args)
         {
-
-            CreateNewFolder("C:\\MePlayGround\\GitHubRepos\\CSharp\\Directory");
+            // Yeni klasör oluşturma.
+            // Creating new folder.
+            //CreateNewFolder("C:\\MePlayGround\\GitHubRepos\\CSharp\\Directory");
+            
+            // Klasör varlık kontrolü. (Var olup olmadığını kontrol ediyoruz.)
+            // The folder existance control.
+            CheckFolderExistance("C:\\MePlayGround\\GitHubRepos\\CSharp\\Directory");
         }
 
+        // Yeni klasör oluştur metodu.
+        // Create new folder method.
         static void CreateNewFolder(string path)
         {
             // The Directory class actually allows us to perform folder operations on the hard disk.
             // Directory sınıfı bizim aslında sabit disk üzerinde klasör işlemleri yapmamıza olanak sağlar.
             DirectoryInfo DI= Directory.CreateDirectory(path);
+        }
+
+
+        static void CheckFolderExistance(string path)
+        {
+            bool control = Directory.Exists(path);
         }
     }
 }
